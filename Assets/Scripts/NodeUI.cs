38d7f1f5b6e0522179f8ada42cb886d46fc33aca
vroumsvroums <91420665+vroumsvroums@users.noreply.github.com>
2021-11-21 20:03:06 +0200

    Upgrade functionality

--- Assets/Scripts/NodeUI.cs
+++ Assets/Scripts/NodeUI.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NodeUI : MonoBehaviour
 {
 
     public GameObject ui;
+
+    public Text upgradeCost;
+    public Button upgradeButton;
 
     private Node target;
 
@@ -13,12 +17,29 @@ public class NodeUI : MonoBehaviour
 
         transform.position = target.GetBuildPosition();
 
+        if (!target.isUpgraded)
+        {
+            upgradeCost.text = "$" + target.turretBlueprint.upgradeCost;
+            upgradeButton.interactable = true;
+        }
+        else
+        {
+            upgradeCost.text = "DONE";
+            upgradeButton.interactable = false;
+        }
+
         ui.SetActive(true);
     }
 
     public void Hide()
     {
         ui.SetActive(false);
+    }
+
+    public void Upgrade()
+    {
+        target.UpgradeTurret();
+        BuildManager.instance.DeselectNode();
     }
 
 }
