bb9a6b2d1d9c2cf84dd41bfd81cce041eff4e73a
vroumsvroums <91420665+vroumsvroums@users.noreply.github.com>
2021-12-13 17:27:54 +0200

    update

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,17 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-
-    public Text roundsText;
-
-    private void OnEnable()
-    {
-        roundsText.text = PlayerStats.Rounds.ToString();
-    }
-
     public void Retry()
     {
         WaveSpawner.EnemiesAlive = 0;
