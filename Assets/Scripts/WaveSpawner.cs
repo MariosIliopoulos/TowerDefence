d2b651b67806383272f8322256427e0cd3d1c128
vroumsvroums <91420665+vroumsvroums@users.noreply.github.com>
2021-12-17 17:16:42 +0200

    update

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -32,7 +32,7 @@ public class WaveSpawner : MonoBehaviour
             return;
         }
 
-        if (waveIndex == waves.Length)
+        if (waveIndex == waves.Length && PlayerStats.Lives > 0)
         {
             gameManager.WinLevel();
             this.enabled = false;
